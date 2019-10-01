1dfc17aa8e7306b664d88f2855071521e4682bf4
vithort <vithort@gmail.com>
2019-10-01 11:59:50 -0300

    Atualização
    Atualizando

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const createError = require('http-errors');
 const CurriculoController = require('./controllers/curriculo-controller');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,4 +25,18 @@ app.get('/curriculo', (req, res, next) => {
 
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`);
+});
+
+// 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
 });
